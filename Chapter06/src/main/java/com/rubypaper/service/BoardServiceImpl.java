7f7d06565744341e2eeb8c5447431c685ef7c9c7
seoheelsh <seoheelsh@gmail.com>
2021-10-23 18:47:30 +0900

    ..

---
+++ Chapter06/src/main/java/com/rubypaper/service/BoardServiceImpl.java
@@ -0,0 +1,37 @@
+package com.rubypaper.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rubypaper.domain.Board;
+import com.rubypaper.persistence.BoardRepository;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+	@Autowired
+	private BoardRepository boardRepo;
+	
+	@Override
+	public List<Board> getBoardList(Board board){
+		return (List<Board>) boardRepo.findAll();
+	}
+	
+	@Override
+	public Board getBoard(Board board) {
+		return null;
+	}
+	
+	@Override
+	public void insertBoard(Board board) {
+	}
+	
+	@Override
+	public void updateBoard(Board board) {
+	}
+	
+	@Override
+	public void deleteBoard(Board board) {
+	}
+}
